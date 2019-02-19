01aed917c4656491d107cf8f1fa2b74d673e917a
Jay Gambetta <jay.gambetta@us.ibm.com>
2019-02-18 21:16:13 -0500

    Backend example

---
+++ examples/jku_backends.py
@@ -0,0 +1,25 @@
+"""Usage examples for the JKY backends"""
+
+from qiskit_jku_provider import JKUProvider
+
+JKU = JKUProvider()
+
+print(JKU.backends())
+
+backend = JKU.get_backend('local_statevector_simulator_jku')
+print(backend)
+
+# gets the name of the backend.
+print(backend.name())
+
+# gets the status of the backend.
+print(backend.status())
+
+# returns the provider of the backend
+print(backend.provider)
+
+# gets the configuration of the backend.
+print(backend.configuration())
+
+# gets the properties of the backend.
+print(backend.properties())
