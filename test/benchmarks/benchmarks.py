f62c3d2f05e540064b61aab173e5e5c2fc5315fc
HartwigB <bauer.hartwig@gmx.at>
2020-03-05 16:25:41 +0100

    cleanup

--- test/benchmarks/benchmarks.py
+++ test/benchmarks/benchmarks.py
@@ -12,21 +12,23 @@ import os
 from qiskit import QuantumCircuit
 from qiskit import execute
 from qiskit_jku_provider import QasmSimulator
-
+from qiskit.compiler import transpile, assemble
 
 class QasmSimulatorJKUBenchmarkSuite:
     """Runs the Basic qasm_simulator tests from Terra on JKU."""
     params = ["grover_12.qasm", "sym9_193.qasm", "plus63mod4096_163.qasm", "z4_268.qasm",
               "clip_206.qasm"]
+    param_names = params
     timeout = 1800
 
     def setup(self, filename):
-        self.seed = 88
         self.backend = QasmSimulator(silent=True)
         full_file = os.path.join(os.path.dirname(__file__), 'qasms', filename)
-        self.circuit = QuantumCircuit.from_qasm_file(full_file)
-        self.circuit.name = filename
-
+        circuit = QuantumCircuit.from_qasm_file(full_file)
+        circuit.name = filename
+        experiments = transpile(circuit, seed_transpiler=34342)
+        self.qobj = assemble(experiments, shots=1024)
+        
     def time_qasm_simulator_single_shot(self, parameter):
         """Test single shot run."""
         result = execute(self.circuit, self.backend, seed_transpiler=34342, shots=1).result()
@@ -34,6 +36,5 @@ class QasmSimulatorJKUBenchmarkSuite:
 
     def time_qasm_simulator(self, parameter):
         """Test data counts output for single circuit run against reference."""
-        shots = 1024
-        result = execute(self.circuit, self.backend, seed_transpiler=34342, shots=shots).result()
+        result = self.backend.run(self.qobj).result()
         assert(result.success)
